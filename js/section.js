8e9f42a2733d088e7a327c2940e361725726fbe3
cake <164044366+cakecable@users.noreply.github.com>
2025-01-06 22:15:12 -0500

    pageswitcher
    Pageswitching javascript

---
+++ js/section.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => section.classList.remove('active'));
+  const targetSection = document.getElementById(sectionId);
+  targetSection.classList.add('active');
+}
+document.addEventListener('DOMContentLoaded', () => {
+  showSection('about');
+});
+
+/* thank you StackOverflow :pray: */
